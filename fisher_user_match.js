3e730c0f0ef9466c0438bf4eac00894436d9c812
Carl Eiserman <carleiserman@gmail.com>
2017-01-26 14:23:47 +0200

    Fixed tests failing for ALL trips logged by managers.

--- fisher_user_match.js
+++ fisher_user_match.js
@@ -1,76 +1,139 @@
+// const pg = require('pg');
+// var defaultClient = new pg.Client();
+
+// var defaultStartDate = new Date("2017-01-24");
+// var defaultEndDate = new Date('2017-01-26');
+
+// defaultStartDate = defaultStartDate.toISOString();
+// defaultEndDate = defaultEndDate.toISOString();
+
+// var dashline="===================";
+// var log = "";
+
+// var DB_URL = "postgres://eaveeikumjabqn:HoOE8hCrYllmUdWI_fwNyi_NN0@ec2-54-247-98-197.eu-west-1.compute.amazonaws.com:5432/d1qik232pvmso9";
+
+// pg.defaults.ssl = true;
+// pg.connect(DB_URL, function(err, client) {
+
+//     if (err) throw err;
+//     console.log('Connected to postgres succesfully \n' + dashline);
+//     log += ('Connected to postgres succesfully \n\n' + dashline);
+
+//     //fisher tests are run where after email is send
+//     runTest(client, defaultStartDate, defaultEndDate, function(var1, var2){
+//     	console.log(var1);
+//     	console.log(var2);
+
+//     })
+
+// });
+
+
+
+//function to run first check of validation
+//this check is to gurantee that the fisher who has submited data from a trip matches up with the username provided on the form.
+//the only exception is a fisher_manager who is allowed to submit someone elses data
+
+function runTest(client, startdate, enddate, callback) {
+
+    var users = [];
+    var users_from_trips = [];
+    var row;
+    var person;
+    var errors = 0;
+
+    //variable that stores all logging info for individual job
+    var LogString = "";
+
+    console.log("Test 2: Fisher Trip Username Match Test:");
+    LogString += "Test 2: Fisher Trip Username Match Test:\n"
+
+
+
+
+    //run a query on the database to pull the main_fisher_id__c and user_id__c fields from the trips table entered in the last 24h
+    client
+        .query('SELECT main_fisher_id__c, user_id__c, sfid FROM salesforce.ablb_fisher_trip__c WHERE lastmodifieddate BETWEEN \'' + startdate + '\' AND \'' + enddate + '\'')
+        .on('row', function(row) {
+            //each row is read from the database and is entered as an object in the array 'users_from_trips'
+            users_from_trips.push(row);
+            //	console.log(row.sfid)
+            //console.log(row.main_fisher_id__c + " " + row.user_id__c )
+            /*	
+
+            	Trip Data:
+            	main_fisher_id__c
+            	user_id__c
+            	sfid
+            	
+            	Users Data:
+            	username
+            	abalobi_id__c
+            	abalobi_usertype__c
+            */
+
+        })
+        //when the rows have been finished output how many trips were made in the last 24h
+        .on('end', function(result) {
+            console.log(result.rowCount + ' records were received');
+            LogString += result.rowCount + ' records were received\n'
+
+            //run another query to recieve all possible username and abalobi_id__c combinations from the database
+            client
+                .query('SELECT username, abalobi_id__c, abalobi_usertype__c FROM salesforce.user')
+                .on('row', function(row) {
+                    //push each row as an object into an array of users
+                    users.push(row);
+                })
+
+            .on('end', function(result) {
+                //scan the array of total users for a match for each of the users_from trip for a match
+                for (person in users_from_trips) {
+                    var match = false;
+                    for (row in users) {
+                        //check if the trip's user corresponds to correct abalobi ID and username
+
+                        //If the main fisher ID matches any user ID, 
+                        //AND the user ID from the trip matches any Username
+                        if (users[row].abalobi_id__c == users_from_trips[person].main_fisher_id__c && users[row].username == users_from_trips[person].user_id__c) {
+                            match = true
+                        } 
+                        else if (users[row].username == users_from_trips[person].user_id__c && users[row].abalobi_usertype__c.includes("fisher_manager")){
+                        	match = true;
+                        	// console.log("MANAGER FOUND");
+                        }	
+                        // else if (((users[row].abalobi_usertype__c).includes("fisher_manager"))){
+                        // 	match = true;
+                        // }
+                        // console.log("USERTYPE: " + users[row].abalobi_usertype__c);
+                    }
+                    //if there is no match and the usertype is not a fisher_manager, output which user is incorrect and increment the total amount not found
+                    if (match == false) {
+                        console.log("\nError @ sfID " + users_from_trips[person].sfid
+                        	+ "\nTrip Fisher ID: " + users_from_trips[person].main_fisher_id__c
+                        	+ "\nDoes not match Trip User ID: " + users_from_trips[person].user_id__c 
+                        	// + "\nSalesforce Username: " + users[row].username
+                        	// + "\nUsertype of logger: " + users[row].abalobi_usertype__c 
+                        	+ "\n");
+                        LogString += "Error @ sfID " + users_from_trips[person].sfid + " https://eu5.salesforce.com/" + users_from_trips[person].sfid + '\n'
+                        errors++;
+                    }
+                }
+                if (errors == 0) {
+                    console.log("0 Errors - Test PASSED \n");
+                    LogString += "0 Errors - Test PASSED \n"
+                    callback(LogString, errors);
+                } else {
+                    //output the total amount of users who are a mismatch
+                    console.log(errors + " Errors - Test FAILED \r\n");
+                    LogString += errors + " Errors - Test FAILED \n"
+                    callback(LogString, errors);
+                }
+
+            })
+        })
+}
+
 module.exports = {
-
-
-	//function to run first check of validation
-	//this check is to gurantee that the fisher who has submited data from a trip matches up with the username provided on the form.
-	//the only exception is a fisher_manager who is allowed to submit someone elses data
-
-	runTest : function(client,  startdate, enddate,  callback){
-
-		var users = [];
-		var users_from_trips = [];
-		var row;
-		var person;
-		var errors = 0;
-
-		//variable that stores all logging info for individual job
-		var LogString = "";
-
-		console.log("Test 2: Fisher Trip Username Match Test:");
-		LogString += "Test 2: Fisher Trip Username Match Test:\n"
-
-		//run a query on the database to pull the main_fisher_id__c and user_id__c fields from the trips table entered in the last 24h
-		client
-		.query('SELECT main_fisher_id__c, user_id__c, sfid FROM salesforce.ablb_fisher_trip__c WHERE lastmodifieddate BETWEEN \'' + startdate + '\' AND \'' + enddate + '\'')
-		.on('row', function(row) {
-			//each row is read from the database and is entered as an object in the array 'users_from_trips'
-			users_from_trips.push(row);
-			//	console.log(row.sfid)
-			//console.log(row.main_fisher_id__c + " " + row.user_id__c )
-		})
-		//when the rows have been finished output how many trips were made in the last 24h
-		.on('end', function(result) {
-			console.log(result.rowCount + ' records were received');
-			LogString += result.rowCount + ' records were received\n'
-
-			//run another query to recieve all possible username and abalobi_id__c combinations from the database
-			client
-			.query('SELECT username, abalobi_id__c, abalobi_usertype__c FROM salesforce.user')
-			.on('row', function(row) {
-				//push each row as an object into an array of users
-				users.push(row);
-			})
-
-			.on('end', function(result) {
-				//scan the array of total users for a match for each of the users_from trip for a match
-				for (person in users_from_trips){
-					var match = false;
-					for (row in users){
-						//check if the trip's user corresponds to correct abalobi ID and username
-						if (users[row].abalobi_id__c == users_from_trips[person].main_fisher_id__c && users[row].username == users_from_trips[person].user_id__c ){
-							match = true
-						}
-					}
-					//if there is no match and the usertype is not a fisher_manager, output which user is incorrect and increment the total amount not found
-					if (match == false && !((users[row].abalobi_usertype__c).includes("fisher_manager"))){
-						console.log("Error @ sfID " + users_from_trips[person].sfid  );
-						LogString += "Error @ sfID " + users_from_trips[person].sfid + " https://eu5.salesforce.com/" + users_from_trips[person].sfid + '\n'
-						errors++;
-					}
-				}
-				if (errors == 0){
-					console.log("0 Errors - Test PASSED \n");
-					LogString +="0 Errors - Test PASSED \n"
-					callback(LogString, errors);
-				}
-				else{
-					//output the total amount of users who are a mismatch
-					console.log(errors + " Errors - Test FAILED \r\n");
-					LogString += errors + " Errors - Test FAILED \n"
-					callback(LogString, errors);
-				}
-
-			})
-		})
-	}
-};
+    runTest: runTest
+}
