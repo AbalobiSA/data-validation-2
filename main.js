24fa4339b4c16c71c24ba793ebe6569c3b690577
Carl Eiserman <carleiserman@gmail.com>
2017-10-23 14:34:54 +0200

    fix: return fishertests function #2

--- main.js
+++ main.js
@@ -28,102 +28,104 @@ let dashline = "-------------------------------------------------\n\n";
  */
 function main() {
 
+    return new Promise((resolve, reject) => {
 
+        // Reset values
+        totalErrors = 0;
+        testsRun = 0;
+        testsFailed = 0;
 
-    // Reset values
-    totalErrors = 0;
-    testsRun = 0;
-    testsFailed = 0;
+        // The subject of the email that will either be 'All okay' or 'some failed' depending on the outcome of tests
+        let jobSubject;
 
-    // The subject of the email that will either be 'All okay' or 'some failed' depending on the outcome of tests
-    let jobSubject;
+        // Create a timestamp in UTC and create master log for tests
+        let timestamp = new Date();
+        let log = "Salesforce validation job started at: " + timestamp + "\n\n";
+        console.log(log);
 
-    // Create a timestamp in UTC and create master log for tests
-    let timestamp = new Date();
-    let log = "Salesforce validation job started at: " + timestamp + "\n\n";
-    console.log(log);
+        // Handle the time period between which the query searches.
+        // If no time period specified default to last 24 hours
+        let currentDate = new Date();
+        let yesterday = new Date();
+        yesterday.setDate(yesterday.getDate() - 1);
 
-    // Handle the time period between which the query searches.
-    // If no time period specified default to last 24 hours
-    let currentDate = new Date();
-    let yesterday = new Date();
-    yesterday.setDate(yesterday.getDate() - 1);
+        let startDate, endDate;
 
-    let startDate, endDate;
+        // If valid argument are entered set them as the start and end date in the query's of the Tests
+        if (process.argv[2] !== undefined || process.argv[3] !== undefined) {
+            startDate = new Date(process.argv[2]);
+            endDate = new Date(process.argv[3]);
+            startDate = startDate.toISOString();
+            endDate = endDate.toISOString();
 
-    // If valid argument are entered set them as the start and end date in the query's of the Tests
-    if (process.argv[2] !== undefined || process.argv[3] !== undefined) {
-        startDate = new Date(process.argv[2]);
-        endDate = new Date(process.argv[3]);
-        startDate = startDate.toISOString();
-        endDate = endDate.toISOString();
+            console.log("Range Date Specified.\nRunning Tests for records between " + startDate + " and " + endDate + " (time in UTC)\n");
+            log += "Range Date Specified.\nRunning Tests for records between " + startDate + " and " + endDate + ' (time in UTC)\n\n'
+        }
+        // Else if no date or invalid dates are given as arguments the end date will be set to the current time and the
+        // start date to 24h before the start date. i.e. will run query for last 24 hours
+        else {
+            startDate = yesterday;
+            endDate = currentDate;
+            startDate = startDate.toISOString();
+            endDate = endDate.toISOString();
 
-        console.log("Range Date Specified.\nRunning Tests for records between " + startDate + " and " + endDate + " (time in UTC)\n");
-        log += "Range Date Specified.\nRunning Tests for records between " + startDate + " and " + endDate + ' (time in UTC)\n\n'
-    }
-    // Else if no date or invalid dates are given as arguments the end date will be set to the current time and the
-    // start date to 24h before the start date. i.e. will run query for last 24 hours
-    else {
-        startDate = yesterday;
-        endDate = currentDate;
-        startDate = startDate.toISOString();
-        endDate = endDate.toISOString();
+            console.log("debug: start date: " + startDate);
+            console.log("debug: end date: " + endDate);
 
-        console.log("debug: start date: " + startDate);
-        console.log("debug: end date: " + endDate);
+            console.log("No Date Range Specified - Defaulting to the last 24h.\nRunning Tests for records between " + startDate + " and " + endDate + " (time in +0:00 UTC)\n");
+            log += "No Date Range Specified - Defaulting to the last 24h.\nRunning Tests for records between " + startDate + " and " + endDate + ' (time in +0:00 UTC)\n\n'
+        }
 
-        console.log("No Date Range Specified - Defaulting to the last 24h.\nRunning Tests for records between " + startDate + " and " + endDate + " (time in +0:00 UTC)\n");
-        log += "No Date Range Specified - Defaulting to the last 24h.\nRunning Tests for records between " + startDate + " and " + endDate + ' (time in +0:00 UTC)\n\n'
-    }
+        /**
+         * Create a connection to Salesforce and start running the checks
+         */
+        salesforce.createConnection().then(client => {
 
-    /**
-     * Create a connection to Salesforce and start running the checks
-     */
-    return salesforce.createConnection().then(client => {
+            // Fisher tests are run where after email is send
+            fisherTests(client, log, startDate, endDate, (test_logs, errors) => {
 
-        // Fisher tests are run where after email is send
-        return fisherTests(client, log, startDate, endDate, (test_logs, errors) => {
+                let finishTime = new Date();
+                let runtime = finishTime.getTime() - timestamp.getTime();
 
-            let finishTime = new Date();
-            let runtime = finishTime.getTime() - timestamp.getTime();
+                log += test_logs;
+                totalErrors += errors;
 
-            log += test_logs;
-            totalErrors += errors;
+                console.log(dashline + "\nJob Finished at: " + finishTime.toISOString() + "\n");
+                log += "Job Finished at: " + finishTime + "\n\n";
+                console.log("Summary: ");
+                log += "Summary: \n";
+                console.log("Tests Run: " + testsRun);
+                log += "Tests Run: " + testsRun + "\n";
+                console.log("Tests Failed: " + testsFailed);
+                log += "Tests Failed: " + testsFailed + "\n";
+                console.log("Total Errors: " + totalErrors);
+                log += "Total Errors: " + totalErrors + "\n";
+                console.log("Runtime: " + runtime / 1000 + " seconds");
+                log += "Runtime: " + runtime / 1000 + " seconds\n";
 
-            console.log(dashline + "\nJob Finished at: " + finishTime.toISOString() + "\n");
-            log += "Job Finished at: " + finishTime + "\n\n";
-            console.log("Summary: ");
-            log += "Summary: \n";
-            console.log("Tests Run: " + testsRun);
-            log += "Tests Run: " + testsRun + "\n";
-            console.log("Tests Failed: " + testsFailed);
-            log += "Tests Failed: " + testsFailed + "\n";
-            console.log("Total Errors: " + totalErrors);
-            log += "Total Errors: " + totalErrors + "\n";
-            console.log("Runtime: " + runtime / 1000 + " seconds");
-            log += "Runtime: " + runtime / 1000 + " seconds\n";
+                if (totalErrors !== 0) {
+                    jobSubject = "Some Failed"
+                } else {
+                    jobSubject = "All OK"
+                }
 
-            if (totalErrors !== 0) {
-                jobSubject = "Some Failed"
-            } else {
-                jobSubject = "All OK"
-            }
+                if (test_logs === STR_NO_RECORDS_RECEIVED) {
+                    jobSubject += " (NO TRIPS RECEIVED)"
+                }
 
-            if (test_logs === STR_NO_RECORDS_RECEIVED) {
-                jobSubject += " (NO TRIPS RECEIVED)"
-            }
+                email.send_report(log, jobSubject, () => {
+                    console.log('Report sent ;)');
+                });
 
-            email.send_report(log, jobSubject, () => {
-                console.log('Report sent ;)');
+                resolve("data validation: successfully run")
             });
-
-            return Promise.resolve("data validation: successfully run")
+        }).catch(err => {
+            console.log(err);
+            console.log('Could not connect to Salesforce. Exiting...');
+            reject(err)
         });
-    }).catch(err => {
-        console.log(err);
-        console.log('Could not connect to Salesforce. Exiting...');
-        return Promise.reject(err)
-    });
+
+    })
 }
 
 /**
